c8c0d168bb0df2f58d2dd5e10f8e0d6845ca969e
mirror <mirror010@hotmail.com>
2020-09-12 21:16:26 +0800

    字体大小调整

--- asset/js/mainmenu.js
+++ asset/js/mainmenu.js
@@ -11,7 +11,7 @@
     <a colorval="1" title="白天">Sun</a>
     <a colorval="2" title="夜晚">Moon</a>
     <a fontval="14" title="fontSize 14px">A</a>
-    <a fontval="16" title="fontSize 16px">AA</a>
-    <a fontval="20" title="fontSize 20px">AAA</a>
+    <a fontval="18" title="fontSize 18px">AA</a>
+    <a fontval="22" title="fontSize 22px">AAA</a>
   </div>
 </div>`;document.querySelector(".layout-part1").innerHTML=t;let n=window.localStorage.getItem("theme");f(n);document.querySelector(".layout-part1").style.backgroundColor=n==2?"#303030":"white";document.querySelectorAll(".theme a[colorval]").forEach(n=>{let t=n.getAttribute("colorval");n.onclick=()=>{i(t)}});document.querySelectorAll(".theme a[fontval]").forEach(n=>{let t=n.getAttribute("fontval");n.onclick=()=>{r(t)}})}function f(n){const i=window.document.querySelector("#logo_canv"),t=i.getContext("2d");t.font="26px bold Arial,Tahoma,Helvetica";t.textBaseline="middle";t.fillStyle=n==2?"#c3cfd3":"#333";n!=2&&(t.shadowColor="#c3cfd3",t.shadowOffsetX=3,t.shadowOffsetY=6);t.fillText("Mirror Space",10,15)}u();let n=window.localStorage.getItem("theme"),t=window.localStorage.getItem("fontsize");n>0&&i(n);t>0&&r(t)})();(()=>{if(location.hostname!="localhost"){let t=document.createElement("link");t.rel="stylesheet";t.href="https://cdn.jsdelivr.net/npm/gitalk@1/dist/gitalk.css";let i=document.createElement("script");i.src="https://cdn.jsdelivr.net/npm/gitalk@1/dist/gitalk.min.js";document.body.append(t);document.body.append(i);let n=document.createElement("div");n.id="gitalk-container";n.style.width="90%";n.style.maxWidth="640px";n.style.margin="0 auto";document.querySelector(".layout").append(n);i.onload=()=>{var n=new Gitalk({clientID:"dbddc2f305d637314ab8",clientSecret:"0e6342b5a033d9e978097fd2e8f0965d8a8c14c5",repo:"mirrortom.github.io",owner:"mirrortom",admin:["mirrortom"],id:location.pathname,distractionFreeMode:!1});n.render("gitalk-container")}}})()
